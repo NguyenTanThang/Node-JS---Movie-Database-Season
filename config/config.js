ea5f4403c3b9d1ce277da1a6968b635747669684
Victor <nguyentanthang9a4@gmail.com>
2020-12-10 11:09:31 +0700

    Version 1.0.1 - Fix the config file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const STRIPE_API_KEY = `sk_test_ETJsviO2O5GerqT9HQ5Tsw9a00f0qbPheK`;
 const MONGODB_URI = `mongodb+srv://test:123456a@tanthang.b7rfb.mongodb.net/test-movies?retryWrites=true&w=majority`;
-//const CURRENT_URL = `https://node-js-movie-database-backend.herokuapp.com`;
-const CURRENT_URL = `http://localhost:5000`;
+const CURRENT_URL = `https://node-js-movie-database-season.herokuapp.com`;
+//const CURRENT_URL = `http://localhost:5000`;
 
-const CURRENT_CLIENT_URL = `https://react-js-movie-database-client.netlify.app/`
+const CURRENT_CLIENT_URL = `https://react-js-movie-database-season-client.netlify.app/`
 
 const getOMDBURL = (IMDB_ID) => {
     return `https://www.omdbapi.com/?i=${IMDB_ID}&apikey=a8ef1841`
